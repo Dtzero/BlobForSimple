626a2f4f04015de9434d58e398e68c1b6770d075
Rezar <yuwenhao@zmeng123.com>
2017-11-14 16:03:01 +0800

    fix bug

---
+++ src/main/java/cn/wenhao/BlobForSimple/App.java
@@ -0,0 +1,13 @@
+package cn.wenhao.BlobForSimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
